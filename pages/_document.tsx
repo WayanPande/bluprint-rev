b239acc18c94766bea8128958b3fd253dd6a6a94
WayanPande <yande554@gmail.com>
2022-11-15 18:01:54 +0800

    remove crossorigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,11 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossorigin
-          ></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Quicksand:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
