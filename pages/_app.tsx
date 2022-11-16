a0a750347fbded6e3646e19b9e7a8ae4967aa26d
WayanPande <yande554@gmail.com>
2022-11-16 23:23:18 +0800

    add page transition and loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,54 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import ProgressBar from "@badrap/bar-of-progress";
+import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const progress = new ProgressBar({
+  size: 5,
+  color: "#45A9DD",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+  useEffect(() => {
+    router.events.on("routeChangeStart", progress.start);
+    router.events.on("routeChangeComplete", progress.finish);
+    router.events.on("routeChangeError", progress.finish);
+
+    return () => {
+      router.events.off("routeChangeStart", progress.start);
+      router.events.off("routeChangeComplete", progress.finish);
+      router.events.off("routeChangeError", progress.finish);
+    };
+  }, [router]);
+
+  const animationTransition = {
+    duration: 1,
+    ease: "easeInOut",
+  };
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      transition={animationTransition}
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </motion.div>
   );
 }
 
