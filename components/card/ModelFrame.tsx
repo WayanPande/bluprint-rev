60fdbd416ec548d68d2708a95cd8d2e808d0564e
WayanPande <yande554@gmail.com>
2022-11-24 10:10:31 +0800

    fix model frame size

--- components/card/ModelFrame.tsx
+++ components/card/ModelFrame.tsx
@@ -10,11 +10,17 @@ interface IProps {
 const ModelFrame: React.FC<IProps> = ({ url, alt = "gambar", className }) => {
   return (
     <div
-      className={`shadow-lg bg-gradient-to-r from-cyan-500 to-blue-500 h-56 w-56 xl:h-[16.5rem] xl:w-[16.5rem] flex justify-center items-center ${className}`}
+      className={`shadow-lg bg-gradient-to-r from-cyan-500 to-blue-500 h-56 w-56 xl:h-[16.5rem] xl:w-[16.5rem] flex justify-center items-center ${className} hover:scale-110 transition-all cursor-pointer hover:shadow-xl`}
     >
       <div className="bg-white h-48 w-48 xl:h-[14.5rem] xl:w-[14.5rem] flex justify-center items-center shadow-inner">
-        <div className="shadow-frame flex justify-center p-2 lg:p-4 xl:p-7">
-          <Image src={url} width={130} height={130} alt={alt} />
+        <div className="shadow-frame flex justify-center p-4 lg:m-4 xl:m-5">
+          <Image
+            src={url}
+            width={130}
+            height={130}
+            alt={alt}
+            className="lg:w-full lg:h-full"
+          />
         </div>
       </div>
     </div>
